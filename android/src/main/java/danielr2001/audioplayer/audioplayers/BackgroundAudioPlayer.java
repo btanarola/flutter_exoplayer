1e10ea51f9c6d2aee2e929fefe7861d5306467c4
Prakash Jyani <prakash.jyani@concettolabs.com>
2019-12-09 11:18:48 +0530

    Try to handle exceptions

--- android/src/main/java/danielr2001/audioplayer/audioplayers/BackgroundAudioPlayer.java
+++ android/src/main/java/danielr2001/audioplayer/audioplayers/BackgroundAudioPlayer.java
@@ -18,6 +18,7 @@ import android.os.IBinder;
 import androidx.annotation.Nullable;
 
 import com.google.android.exoplayer2.C;
+import com.google.android.exoplayer2.ExoPlaybackException;
 import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.Player;
 import com.google.android.exoplayer2.SimpleExoPlayer;
@@ -35,6 +36,7 @@ import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSource;
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
+import com.google.android.exoplayer2.util.Log;
 import com.google.android.exoplayer2.util.Util;
 
 import java.util.ArrayList;
@@ -370,6 +372,11 @@ public class BackgroundAudioPlayer implements AudioPlayer {
                     } 
                     // handle of released is in release method!
                 }
+            }
+
+            @Override
+            public void onPlayerError(ExoPlaybackException error) {
+                Log.e("error", "error in load data********");
             }
         });
     }
